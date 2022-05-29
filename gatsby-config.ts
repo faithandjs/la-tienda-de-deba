6f06cca52019ef0b8598694e45b1afe429bc73cc
Faith <faiththe1rst@gmail.com>
2022-05-29 22:00:55 +0100

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,8 +3,17 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        password: process.env.SHOPIFY_APP_PASSWORD,
+        storeUrl: process.env.GATSBY_MYSHOPIFY_URL,
+        salesChannel: process.env.SHOPIFY_APP_ID, // Optional but recommended
+      },
+    },
+    'gatsby-plugin-image',
+  ],
   jsxRuntime: `automatic`,
 };
-
 export default config;
